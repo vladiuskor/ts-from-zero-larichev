033f42ae1de52cf0fe563c648360222f975c03b8
Vlad Korobko <max.vlad.kor@gmail.com>
2023-02-21 17:54:28 +0200

    Resolved firs exercise

---
+++ test.js
@@ -0,0 +1,11 @@
+var info = {
+    officeId: 45,
+    isOpened: false,
+    contacts: {
+        phone: "+79100000000",
+        email: "my@email.ua",
+        address: {
+            city: "Kiyv"
+        }
+    }
+};
